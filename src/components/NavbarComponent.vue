940656fc9c9f885a303705191197ef214497c327
JosueACG17 <cgjosue17@gmail.com>
2025-03-18 08:54:30 -0500

    Update Views

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="border-gray-200 bg-black">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <RouterLink to="/home" class="flex items-center space-x-3 rtl:space-x-reverse text-white">
+      <RouterLink to="/inicio" class="flex items-center space-x-3 rtl:space-x-reverse text-white">
         <img src="https://upload.wikimedia.org/wikipedia/commons/5/50/Bitcoin.png" class="h-8" alt="Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap">SpendWise</span>
       </RouterLink>
@@ -12,28 +12,37 @@
           <img class="w-8 h-8 rounded-full" src="https://cdn-icons-png.flaticon.com/512/219/219983.png"
             alt="user photo" />
         </button>
+        <!-- Icono de notificaciones -->
+         <RouterLink to="/notificaciones">
+        <div class="relative cursor-pointer sm:ml-4" >
+          <BellIcon class="w-6 h-6 text-white" />
+          <span v-if="totalNotificaciones > 0"
+            class="absolute -top-1 -right-1 bg-red-500 text-white text-xs font-bold rounded-full w-4 h-4 flex items-center justify-center">
+            {{ totalNotificaciones }}
+          </span>
+        </div>
+      </RouterLink>
         <div v-show="isDropdownOpen"
-            class="z-50 absolute right-0 w-56 top-10 sm:top-8 my-4 text-base list-none divide-y rounded-lg shadow-sm bg-black divide-gray-600">
-            <div class="p-4 flex items-center space-x-3 border-b border-gray-200 ">
-              <img class="h-12 w-12 rounded-full border-2 border-gray-500 p-0.5"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt="User avatar" />
-              <div>
-                <p class="text-sm font-medium text-white ">Josue Chan</p>
-                <p class="text-xs text-white">cgjosue19@gmail.com</p>
-              </div>
+          class="z-50 absolute right-0 w-56 top-10 sm:top-8 my-4 text-base list-none divide-y rounded-lg shadow-sm bg-black divide-gray-600">
+          <div class="p-4 flex items-center space-x-3 border-b border-gray-200 ">
+            <img class="h-12 w-12 rounded-full border-2 border-gray-500 p-0.5"
+              src="https://cdn-icons-png.flaticon.com/512/219/219983.png" alt="User avatar" />
+            <div>
+              <p class="text-sm font-medium text-white ">Josue Chan</p>
+              <p class="text-xs text-white">cgjosue19@gmail.com</p>
             </div>
-            <RouterLink to="/profile" class="flex items-center px-4 py-2 text-sm text-white  hover:bg-gray-600 ">
-              <UserIcon class="h-5 w-5 mr-2" />
-              Perfil
-            </RouterLink>
-
-            <button @click="confirmLogout"
-              class="flex items-center w-full text-left px-4 py-2 text-sm text-white hover:bg-gray-600 cursor-pointer">
-              <ArrowRightOnRectangleIcon class="h-5 w-5 mr-2" />
-              Cerrar sesión
-            </button>
           </div>
+          <RouterLink to="/profile" class="flex items-center px-4 py-2 text-sm text-white  hover:bg-gray-600 ">
+            <UserIcon class="h-5 w-5 mr-2" />
+            Perfil
+          </RouterLink>
+
+          <button @click="confirmLogout"
+            class="flex items-center w-full text-left px-4 py-2 text-sm text-white hover:bg-gray-600 cursor-pointer">
+            <ArrowRightOnRectangleIcon class="h-5 w-5 mr-2" />
+            Cerrar sesión
+          </button>
+        </div>
         <button type="button"
           class="inline-flex items-center p-2 sm:w-10 sm:h-10 w-8 h-8 justify-center text-sm text-gray-300 rounded-lg md:hidden hover:bg-gray-700 cursor-pointer"
           aria-controls="navbar-user" aria-expanded="false" @click="toggleMenu">
@@ -47,9 +56,9 @@
         <ul
           class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
           <li>
-            <RouterLink to="/home"
+            <RouterLink to="/inicio"
               class="block py-2 px-3 rounded-sm sm:hover:bg-transparent md:p-0 sm:hover:text-yellow-500 "
-              :class="{ 'text-yellow-500': $route.path === '/home', 'text-white hover:bg-yellow-600': $route.path !== '/home' }">
+              :class="{ 'text-yellow-500': $route.path === '/inicio', 'text-white hover:bg-yellow-600': $route.path !== '/inicio' }">
               Inicio
             </RouterLink>
           </li>
@@ -88,13 +97,18 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import Swal from 'sweetalert2';
 import { useAuthStore } from '@/stores/authStore';
 import {
   UserIcon,
   ArrowRightOnRectangleIcon,
+  BellIcon
 } from '@heroicons/vue/24/outline';
+import { useNotificacionesStore } from "@/stores/notificationStore";
+
+const notificacionesStore = useNotificacionesStore();
+const totalNotificaciones = computed(() => notificacionesStore.total);
 
 const isDropdownOpen = ref(false);
 const isMenuOpen = ref(false);
---
+++ src/views/User/LabelsView.vue
@@ -0,0 +1,8 @@
+<template>
+  <EtiquetasComponents/>
+</template>
+<script lang="ts" setup>
+import EtiquetasComponents from './components/LabelsComponents.vue';
+</script>
+
+<style scoped></style>
