729bb7171d37418b33614550cdf3cd071c925fcb
JosueACG17 <cgjosue17@gmail.com>
2025-03-26 12:42:33 -0500

    UpdateViews

---
+++ src/views/User/ChartsView.vue
@@ -0,0 +1,12 @@
+<template>
+  <NavbarComponent />
+  <FinancialChartComponent />
+  <FooterComponent />
+</template>
+<script lang="ts" setup>
+import FinancialChartComponent from './components/FinancialChartComponent.vue';
+import NavbarComponent from '@/components/NavbarComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
+</script>
+
+<style scoped></style>
